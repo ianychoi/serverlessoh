37ad46c13c02813bb0f8f551066fe821ec0d459e
Caglar Kara <cakara@microsoft.com>
2022-03-23 17:39:21 +0400

    fixed the issue with final json

--- GetRatings/__init__.py
+++ GetRatings/__init__.py
@@ -1,22 +1,26 @@
 import logging
 import json
 import azure.functions as func
-
+import ast
 
 def main(req: func.HttpRequest, ratings: func.DocumentList) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
 
 #    name = req.params.get('name')
     if not ratings:
-        return func.HttpResponse(f"Hello, nothing found",status_code=200)
+        return func.HttpResponse(f"Hello, nothing found",status_code=404)
     else:
+        
         returnList = []
         for rating in ratings:
-            returnList.append(rating.to_json())
+             ratingJson = rating.to_json()
+             convertTrickyJson = ast.literal_eval(ratingJson)
+             returnList.append(convertTrickyJson)
 
         return func.HttpResponse(                            
-             json.dumps(returnList),
-             status_code=200
+             str(returnList),
+             status_code=200,
+             mimetype="application/json"
         )
 
 
